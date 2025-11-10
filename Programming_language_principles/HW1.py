a3f5391e0d128aed548c88f7ae53b8c219686d4d
SaharTec <sahary153@gmail.com>
2025-11-10 16:56:31 +0200

    task 4 and 5

--- Programming_language_principles/HW1.py
+++ Programming_language_principles/HW1.py
@@ -1,32 +1,62 @@
-#task1
-def print_next_date(date):
-
-    d = int(date / 1000000)
-    m = int(((date / 10000)- day * 100))
-    y = int(date % 10000)
-    print (d, m, y)
-
-    def num_of_days_in_mounth(m):
-        if(m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
-            day_in_m = 31
-        elif (m == 4 or m == 6 or m == 9 or m == 11):
-            day_in_m = 30
-        elif (m == 2):
-            if(is_leap_year(y) == True ):
-                day_in_m = 29
-            else:
-                day_in_m = 28    
-
-
-    def is_leap_year (year):
-        if((year % 4 == 0 and year % 100 != 0) or year & 400 == 0):
+# --------------------------------------------
+#                   Q1
+# --------------------------------------------
+def is_leap_year( year ):
+    if((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
             return True
+    
+def is_last_day( d, m, y ):
+     if m in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+     elif m in [4, 6, 9, 11]:
+        return 30
+     elif m == 2:
+        return 29 if is_leap_year(y) else 28
+
+def print_next_date( date ):
+    d = int(date / 1000000)
+    m = int((date / 10000) % 100)
+    y = int(date % 10000)
+
+    print( "Source Date: "f"{d:02d}/{m:02d}/{y}")
+
+    if(d == is_last_day(d , m ,y)):
+        if(m == 12):
+            d = 1
+            m = 1
+            y = y + 1
+        else:
+            d = 1
+            m = m + 1 
+    else:
+        d = d + 1
+
+    print("Next Day: "f"{d:02d}/{m:02d}/{y}")
 
 
-print(print_next_date(28102025))
+# --------------------------------------------
+'''
+>>> print_next_date( 28102025 )
+Source Date: 28/10/2025
+Next Day:    29/10/2025
+>>> print_next_date( 31102025 )
+Source Date: 31/10/2025
+Next Day:    01/11/2025
+>>> print_next_date( 28022024 )
+Source Date: 28/02/2024
+Next Day:    29/02/2024
+>>> print_next_date( 28022026 )
+Source Date: 28/02/2026
+Next Day:    01/03/2026
+>>> print_next_date( 31122025 )
+Source Date: 31/12/2025
+Next Day:    01/01/2026
+'''
 
-
-def arange_digits(num):
+# --------------------------------------------
+#                   Q2
+# --------------------------------------------
+def arange_digits( num ):
     """bulding 2 string that represent odd and even numbers"""
     evenstr = ""
     oddStr= ""
@@ -43,34 +73,228 @@ def arange_digits(num):
     new_num = evenstr + oddStr
     """casting the string into int"""
     return int(new_num)
-print(arange_digits(1234567))
 
-#task3
-"""check if a given number in range is prime or not"""
+# --------------------------------------------
+'''
+>>> print("Number %d after arange digits: %d" % (1234567,arange_digits(1234567)))
+Number 1234567 after arange digits: 2461357
+>>> print("Number %d after arange digits: %d" % ( 2804,arange_digits(2804)))
+Number 2804 after arange digits: 2804
+>>> print("Number %d after arange digits: %d" % (5713,arange_digits(5713)))
+Number 5713 after arange digits: 5713
+'''
+
+# --------------------------------------------
+#                   Q3
+# --------------------------------------------
+""""if the number is prime it will print it"""
+def print_prime_numbers( ):
+    min, max = 100 , 200
+    print("Prime numbers [100,200]: ", end="")
+    for n in range(min,max+1):
+        if(is_prime(n) == True):
+            print(n , end=",")
+    """check if a given number in range is prime or not"""
 def is_prime(num):
     for n in range(2, num + 1):
         if( num % n == 0):
             return False
         return True
-""""if the number is prime it will print it"""
-def print_prime_numbers(min, max):
-    for n in range(min,max+1):
-        if(is_prime(n) == True):
-            print(n)
 
-#print_prime_numbers(100, 200)
-
-#task4
-def print_figure(size):
-    for space in range(1, ((size +1)//2)+1):
-        if(space == (size+1)// 2):
-            print("*")
-        else:
-            print(end="  ")
     
-    if( size >= 3 and size <= 13):
-        for n in range(1, size +1):
-            for i in range(1, n+1):
-                print("*" , i, end= "")
-            print(" *")
-print_figure(3)
+# --------------------------------------------
+'''
+>>> print_prime_numbers( )
+Prime numbers [100,200]: 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 
+'''
+
+# --------------------------------------------
+#                   Q4
+# --------------------------------------------
+def print_figure( n ):
+    width = 2 * n + 1
+    print("*".center(width))
+
+    for num in range(1, n):
+        line = "*"
+
+        for i in range(1 ,num + 1):
+            line += str(i)
+        
+        for i in range(num - 1, 0, -1):
+            line += str(i)
+
+        line += "*"
+        print(line.center(width))
+
+    print("*" * width) 
+        
+# --------------------------------------------
+'''
+>>> print_figure( 11 )
+          *
+         *1*
+        *121*
+       *12321*
+      *1234321*
+     *123454321*
+    *12345654321*
+   *1234567654321*
+  *123456787654321*
+ *12345678987654321*
+*********************
+'''
+# --------------------------------------------
+#                   Q5
+# --------------------------------------------
+def count_digits( n ):
+   if (n < 10):
+       return 1
+   else:
+       return 1 + count_digits(n // 10)
+def max_digit( n ):
+    if(count_digits(n) == 1):
+        return n
+def num_weight( n ):
+    pass
+
+print(count_digits( 351 ))
+# --------------------------------------------
+'''
+>>> count_digits( 351 )
+3
+>>> max_digit( 351 )
+5
+>>> num_weight( 7145 )
+11
+>>> num_weight( 15 )
+7
+>>> num_weight ( 351 )
+8
+'''
+# --------------------------------------------
+#                   Q6
+# --------------------------------------------
+def is_power2( n ):
+    pass
+# --------------------------------------------
+'''
+>>> is_power2( 1024 )
+True
+>>> is_power2( 1000 )
+False
+'''
+# --------------------------------------------
+#                   Q7
+# --------------------------------------------
+def reduce( n ):
+    pass
+# ------------------------------------------------
+'''
+>>> reduce( -160760 )
+-1676
+>>> reduce( 1020034000 )
+1234
+'''
+
+# --------------------------------------------
+#                   Q8
+# --------------------------------------------
+def pascal( n, m ):
+    pass
+# ------------------------------------------------
+'''
+>>> pascal( 5, 1 )
+5
+>>> pascal( 3, 2 )
+3
+>>> pascal( 10, 4 )
+210
+>>> pascal( 5, 3 )
+10
+>>> pascal( 5, 2 )
+10
+>>> pascal( 5, 13 )
+-1
+'''
+
+# --------------------------------------------
+#                   main
+# --------------------------------------------
+def main( ):
+    print("<<< Q#1 >>>")
+    print_next_date( 28102025 )
+    print_next_date( 31102025 )
+    print_next_date( 28022024 )
+    print_next_date( 28022026 )
+    print_next_date( 31122025 )
+    print("<<< Q#2 >>>")
+    print("Number %d after arange digits: %d" % (1234567,arange_digits(1234567)))
+    print("Number %d after arange digits: %d" % ( 2804,arange_digits(2804)))
+    print("Number %d after arange digits: %d" % (5713,arange_digits(5713)))
+    print("<<< Q#3 >>>")
+    print_prime_numbers( )
+    print("<<< Q#4 >>>")
+    print_figure( 11 )
+    print("<<< Q#5 >>>")
+    print( count_digits( 351 ) )
+    print( max_digit( 351 ) )
+    print( num_weight( 7145 ), end = ', ' )
+    print( num_weight( 15 ), end = ', ' )
+    print( num_weight ( 351 ) )
+    print("<<< Q#6 >>>")
+    print( is_power2( 1024 ), end = ', ')
+    print( is_power2( 1000 ) )
+    print("<<< Q#7 >>>")
+    print( reduce( -160760 ), end = ', ' )
+    print( reduce( 1020034000 ) )    
+    print("<<< Q#8 >>>")
+    print( pascal( 5, 1 ), end = ', ')
+    print( pascal( 3, 2 ), end = ', ' )
+    print( pascal( 10, 4 ), end = ', ')
+    print( pascal( 5, 3 ), end = ', ')
+    print( pascal( 5, 2 ), end = ', ')
+    print( pascal( 5, 13 ) )
+
+# --------------------------------------------
+'''
+<<< Q#1 >>>
+Source Date: 28/10/2025
+Next Day:    29/10/2025
+Source Date: 31/10/2025
+Next Day:    01/11/2025
+Source Date: 28/02/2024
+Next Day:    29/02/2024
+Source Date: 28/02/2026
+Next Day:    01/03/2026
+Source Date: 31/12/2025
+Next Day:    01/01/2026
+<<< Q#2 >>>
+Number 1234567 after arange digits: 2461357
+Number 2804 after arange digits: 2804
+Number 5713 after arange digits: 5713
+<<< Q#3 >>>
+Prime numbers [100,200]: 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 
+<<< Q#4 >>>
+          *
+         *1*
+        *121*
+       *12321*
+      *1234321*
+     *123454321*
+    *12345654321*
+   *1234567654321*
+  *123456787654321*
+ *12345678987654321*
+*********************
+<<< Q#5 >>>
+3
+5
+11, 7, 8
+<<< Q#6 >>>
+True, False
+<<< Q#7 >>>
+-1676, 1234
+<<< Q#8 >>>
+5, 3, 210, 10, 10, -1
+'''
